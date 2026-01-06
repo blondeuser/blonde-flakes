a1432cdeae79ce9b29d4cfcbcafcbc1abd2617b9
blondeuser <vihao.cloud2@gmail.com>
2026-01-06 13:08:41 +0700

    update

--- configuration.nix
+++ configuration.nix
@@ -58,7 +58,8 @@ boot.kernelPackages = pkgs.linuxPackages_zen;
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-
+  # Enable tailscale 
+  services.tailscale.enable = true;
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
